f11beda3827bdfcbab3a3dc24a6923ab20119229
Alex Travagin Chatalov <alex.chatalov@matera.com>
2020-09-03 18:11:42 -0300

    removido a classe de teste

--- src/main/java/com/processSalesFiles/salesFile/threads/Teste.java
+++
@@ -1,9 +0,0 @@
-package com.processSalesFiles.salesFile.threads;
-
-public class Teste {
-    public static void main(String[] args) {
-        SalesThread thread = new SalesThread();
-        thread.start();
-
-    }
-}
